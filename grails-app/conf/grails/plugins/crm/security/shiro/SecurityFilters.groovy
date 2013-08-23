ce02ec2a75366b9ef010f9e8f49cff33b5471c37
Goran Ehrsson <goran@technipelago.se>
2013-08-23 19:32:04 +0200

    Moved SecurityFilters to a package

---
+++ grails-app/conf/grails/plugins/crm/security/shiro/SecurityFilters.groovy
@@ -0,0 +1,93 @@
+package grails.plugins.crm.security.shiro
+
+import org.apache.shiro.SecurityUtils
+import grails.plugins.crm.core.TenantUtils
+import grails.plugins.crm.core.CrmTenantFilters
+
+class SecurityFilters {
+
+    def grailsApplication
+
+    def dependsOn = [CrmTenantFilters]
+
+    def blockedControllers
+    def publicControllers
+    def protectedControllers
+
+    def onNotAuthenticated(subject, filter) {
+        // Check if the request is an Ajax one.
+        def contentType = filter.request.contentType
+        if (filter.request.xhr || contentType?.equalsIgnoreCase("text/xml") || contentType?.equalsIgnoreCase("application/json")) {
+            filter.response.sendError(401) // TODO Render Ajax login
+            return false
+        }
+        return true
+    }
+
+    def filters = {
+        // Ensure that all controllers and actions require an authenticated user,
+        // except for the "public" controller
+        auth(uri: "/**") {
+            before = {
+                // Ignore direct views (e.g. the default main index page).
+                if (!controllerName) return true
+
+                // Check blocked controllers in Config.groovy
+                if(blockedControllers == null) {
+                    blockedControllers = grailsApplication.config.crm.security.controllers.blocked ?: false
+                }
+                if (blockedControllers && blockedControllers.find {match(it, controllerName, actionName)}) {
+                    response.sendError(grailsApplication.config.crm.security.blocked.status ?: 404)
+                    return false
+                }
+
+                // Exclude public controllers in Config.groovy
+                if(publicControllers == null) {
+                    publicControllers = grailsApplication.config.crm.security.controllers.public ?: false
+                }
+                if (publicControllers && publicControllers.find {match(it, controllerName, actionName)}) {
+                    return true
+                }
+
+                // Check protected controllers in Config.groovy
+                if(protectedControllers == null) {
+                    protectedControllers = grailsApplication.config.crm.security.controllers.protected ?: false
+                }
+                if (protectedControllers && protectedControllers.find {match(it, controllerName, actionName)} && SecurityUtils.subject?.authenticated) {
+                    return true
+                }
+
+                // Access control by convention.
+                accessControl {
+                    // Check that the user has the required permission for the target controller/action.
+                    def permString = new StringBuilder()
+                    permString << controllerName << ':' << (actionName ?: "index")
+
+                    // Add the ID if it's in the web parameters.
+                    if (params.id) permString << ':' << params.id
+                    def subject = SecurityUtils.subject
+                    def t0 = System.nanoTime()
+                    def status = subject?.isPermitted(permString.toString())
+                    if(log.isDebugEnabled()) {
+                        log.debug "${status ? '+' : '!'} ${subject?.principal}@${TenantUtils.tenant} $controllerName/$actionName -> $permString (${((System.nanoTime() - t0) / 1000000).intValue()} ms)"
+                    }
+                    return status
+                }
+            }
+        }
+    }
+
+    private boolean match(String ctrl, String controllerName, String actionName) {
+        def (c, a) = ctrl.split(':').toList()
+        if (c == controllerName) {
+            if (a) {
+                if (a.split(',').contains(actionName)) {
+                    return true
+                }
+            } else {
+                return true
+            }
+        }
+        return false
+    }
+}
