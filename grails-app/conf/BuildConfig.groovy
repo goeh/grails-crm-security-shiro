5594d0180fc7aa307bac2c8cffad410788a19628
Goran Ehrsson <goran@technipelago.se>
2014-07-01 21:45:05 +0200

    Added back grails.project.class.dir

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,4 +1,7 @@
 grails.project.work.dir = "target"
+grails.project.class.dir = "target/classes"
+grails.project.test.class.dir = "target/test-classes"
+grails.project.test.reports.dir = "target/test-reports"
 grails.project.target.level = 1.6
 
 grails.project.repos.default = "crm"
