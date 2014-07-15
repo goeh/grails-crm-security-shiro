698f7d6a100a4338d0b48502d95292a62293390f
Goran Ehrsson <goran@technipelago.se>
2014-07-15 22:09:16 +0200

    Version 2.0.0

--- CrmSecurityShiroGrailsPlugin.groovy
+++ CrmSecurityShiroGrailsPlugin.groovy
@@ -1,25 +1,24 @@
 /*
-*  Copyright 2012 Goran Ehrsson.
-*
-*  Licensed under the Apache License, Version 2.0 (the "License");
-*  you may not use this file except in compliance with the License.
-*  You may obtain a copy of the License at
-*
-*       http://www.apache.org/licenses/LICENSE-2.0
-*
-*  Unless required by applicable law or agreed to in writing, software
-*  distributed under the License is distributed on an "AS IS" BASIS,
-*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-*  See the License for the specific language governing permissions and
-*  limitations under the License.
-*  under the License.
-*/
+ * Copyright (c) 2014 Goran Ehrsson.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 import grails.plugins.crm.security.shiro.ShiroSecurityDelegate
 
 class CrmSecurityShiroGrailsPlugin {
-    def groupId = "grails.crm"
-    def version = "1.2.7"
+    def groupId = "gr8crm"
+    def version = "2.0.0"
     def grailsVersion = "2.2 > *"
     def dependsOn = [:]
     def loadAfter = ['crmSecurity', 'shiro']
@@ -33,7 +32,7 @@ class CrmSecurityShiroGrailsPlugin {
     def description = '''
 This plugin leverage the shiro plugin to authenticate and authorize GR8 CRM users.
 '''
-    def documentation = "https://github.com/goeh/grails-crm-security-shiro"
+    def documentation = "http://gr8crm.github.io/plugins/crm-security-shiro/crm-security-shiro.html"
     def license = "APACHE"
     def organization = [name: "Technipelago AB", url: "http://www.technipelago.se/"]
     def issueManagement = [system: "github", url: "https://github.com/goeh/grails-crm-security-shiro/issues"]
